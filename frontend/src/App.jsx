0f8757e7d6eac3ec98a6453c9fee67a6476d8e7b
Adil <skeezygod@gmail.com>
2024-02-24 23:57:30 +0600

    feat: Main page layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "./App.css";
+import WeatherComponent from "./components/WeatherComponent";
+
+function App() {
+  return (
+    <>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        <main className="mt-10 flex-1">
+          <WeatherComponent />
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
